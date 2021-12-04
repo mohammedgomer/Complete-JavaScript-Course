5429f3ecf922e8f0943b72dfb55ff6032b6b96d9
mohammedgomer <mg.omer@outlook.com>
2021-12-04 14:12:32 +0000

    Equality Operators assignment complete

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -457,8 +457,23 @@ if (favourite !== 23) console.log('Why not 23?');
 
 // Assignment - Equality Operators: == vs. === //
 
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+if (numNeighbours === 1) {
+  console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+  console.log('More than 1 border');
+} else {
+  console.log('No borders');
+}
+
+
 
 */
+
+
+
+
 
 /////////////////////// Boolean Logic ///////////////////////
 
