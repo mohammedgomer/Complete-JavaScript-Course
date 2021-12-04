ce25b187e425437d0068ab3a315f569e250c6698
mohammedgomer <mg.omer@outlook.com>
2021-12-04 14:43:01 +0000

    The switch Statement assignment complete

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -597,8 +597,30 @@ if (day === 'Monday') {
 
 // Assignment - The switch Statement //
 
+const language = 'Mandarin';
 
+switch (language) {
+  case 'Chinese':
+  case 'Mandarin':
+    console.log('MOST number of native speakers');
+    break;
+  case 'Spanish':
+    console.log('2nd place in number of native speakers');
+    break;
+  case 'English':
+    console.log('3rd place');
+    break;
+  case 'Hindi':
+    console.log('Number 4');
+    break;
+  case 'Arabic':
+    console.log('5th most spoken language');
+    break;
+  default: console.log('Great language too :D');
+}
 */
+
+
 
 /////////////////////// Statements & Expressions ///////////////////////
 
